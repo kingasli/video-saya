51034b0aea64d28d5b591756f720d6365e3558e9
kingasli <kingkep123@gmail.com>
2025-08-25 15:30:32 +0700

    asutolol

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,11 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        // Path yang akan cocok di Next.js
         source: '/api/:path*',
-        // URL tujuan yang merupakan Worker API Anda
         destination: 'https://kitacoba.kingkep123.workers.dev/api/:path*',
       },
     ];
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig; // Ganti "module.exports" menjadi "export default"
