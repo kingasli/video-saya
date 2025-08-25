20855a14c5624f18ed0fa5a8bcca3f496e3d085d
kingasli <kingkep123@gmail.com>
2025-08-25 09:17:00 +0700

    perubahan

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Tambahkan baris ini di bagian paling atas file Anda
+export const runtime = 'edge'; 
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name: 'John Doe' });
 }
