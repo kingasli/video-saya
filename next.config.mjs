715d5d19b21b0192a122027206b2bf1c084980c4
kingasli <kingkep123@gmail.com>
2025-08-25 09:28:09 +0700

    Menambah edge runtime 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@cloudflare/next-on-pages'],
+    runtime: 'edge', // Ini adalah baris penting yang kita tambahkan
+  },
 };
 
 export default nextConfig;
